d34d0eb999f91501faf2262d2096015977b0e46c
James Cleveland <jc@blit.cc>
2021-01-06 17:26:13 +0000

    use cloudfront for blit

--- lib/blit-stack.ts
+++ lib/blit-stack.ts
@@ -1,16 +1,23 @@
-import * as apigw from "@aws-cdk/aws-apigateway";
 import * as acm from "@aws-cdk/aws-certificatemanager";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import { SecurityPolicyProtocol } from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 import * as route53 from "@aws-cdk/aws-route53";
 import * as cdk from "@aws-cdk/core";
 
 const vpsHost = "149.91.89.243";
 const zoneName = "blit.cc";
+const internal = "xxpk4shiicfjldb50oiasudnas3nd";
+const internalPort = 10080;
 
 // We could make this in CDK but apparently this is not recommended.
 const certArn =
   "arn:aws:acm:eu-west-2:339435723451:certificate/4a0beb44-1d59-494e-a8e1-f1f47c74b61e";
 
-const enableGateway = true;
+const certArnUSEast =
+  "arn:aws:acm:us-east-1:339435723451:certificate/424891c1-18a9-4579-a5df-faa6513a32e1";
+
+const proxy = true;
 const createCert = false;
 
 export class BlitStack extends cdk.Stack {
@@ -23,13 +30,19 @@ export class BlitStack extends cdk.Stack {
 
     const vpsTarget = route53.RecordTarget.fromIpAddresses(vpsHost);
 
-    new route53.ARecord(this, "BlitWildcard", {
+    new route53.ARecord(this, "BlitInternal", {
       zone,
-      recordName: "*",
+      recordName: internal,
       target: vpsTarget,
     });
 
-    if (!enableGateway) {
+    new route53.ARecord(this, "BlitNavidrone", {
+      zone,
+      recordName: "navidrome",
+      target: vpsTarget,
+    });
+
+    if (!proxy) {
       new route53.ARecord(this, "BlitRoot", {
         zone,
         target: vpsTarget,
@@ -57,32 +70,23 @@ export class BlitStack extends cdk.Stack {
       });
     }
 
-    let certificate = acm.Certificate.fromCertificateArn(
-      this,
-      "BlitWildcardCert",
-      certArn
-    );
-
-    if (createCert) {
-      certificate = new acm.Certificate(this, "BlitCert", {
-        domainName: "blit.cc",
-        subjectAlternativeNames: ["*.blit.cc"],
-        validation: acm.CertificateValidation.fromDns(zone),
-      });
-    }
-
-    if (enableGateway) {
-      const gateway = new apigw.RestApi(this, "BlitGateway", {
-        domainName: {
-          domainName: "blit.cc",
-          certificate,
+    if (proxy) {
+      new cloudfront.Distribution(this, "BlitFront", {
+        certificate: acm.Certificate.fromCertificateArn(
+          this,
+          "BlitCertUSEast",
+          certArnUSEast
+        ) as any,
+        domainNames: ["blit.cc"],
+        enableIpv6: true,
+        httpVersion: cloudfront.HttpVersion.HTTP2,
+        minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2019,
+        defaultBehavior: {
+          origin: new origins.HttpOrigin(`${internal}.blit.cc`, {
+            protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+            httpPort: internalPort,
+          }),
         },
-      });
-
-      gateway.root.addProxy({
-        defaultIntegration: new apigw.HttpIntegration(
-          `http://${vpsHost}:10080`
-        ),
       });
     }
   }
