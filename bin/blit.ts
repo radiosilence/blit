ba403bea608376efea75751bacec2c05d7f8c749
James Cleveland <jc@blit.cc>
2021-01-07 16:00:22 +0000

    fixing zones

--- bin/blit.ts
+++ bin/blit.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { BlitStack } from '../lib/blit-stack';
+import * as cdk from "@aws-cdk/core";
+import "source-map-support/register";
+import { BlitStack } from "../lib/blit-stack";
 
 const app = new cdk.App();
-new BlitStack(app, 'BlitStack');
+new BlitStack(app, "BlitStack", {
+  env: {
+    region: "us-east-1",
+    account: "339435723451",
+  },
+});
