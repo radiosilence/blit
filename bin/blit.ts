32b635191d9458457c155af6f853138b1bc96701
James Cleveland <jc@blit.cc>
2021-01-07 23:04:15 +0000

    hopefully

--- bin/blit.ts
+++ bin/blit.ts
@@ -5,6 +5,12 @@ import { BlitStack } from "../lib/blit-stack";
 
 const app = new cdk.App();
 new BlitStack(app, "BlitStack", {
+  vpsHost: "149.91.89.243",
+  zoneName: "blit.cc",
+  internal: "xxpk4shiicfjldb50oiasudnas3nd",
+  navidromePort: 4533,
+  useCloudFront: true,
+
   env: {
     region: "us-east-1",
     account: "339435723451",
