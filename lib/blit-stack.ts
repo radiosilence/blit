32619a0204cb6e1ba604726529c4e1348433225a
James Cleveland <jc@blit.cc>
2021-01-07 23:04:15 +0000

    hopefully

--- lib/blit-stack.ts
+++ lib/blit-stack.ts
@@ -3,18 +3,22 @@ import * as cloudfront from "@aws-cdk/aws-cloudfront";
 import * as origins from "@aws-cdk/aws-cloudfront-origins";
 import * as route53 from "@aws-cdk/aws-route53";
 import * as alias from "@aws-cdk/aws-route53-targets";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 import * as cdk from "@aws-cdk/core";
 
-const vpsHost = "149.91.89.243";
-const zoneName = "blit.cc";
-const internal = "xxpk4shiicfjldb50oiasudnas3nd";
-const internalPort = 10080;
-const navidromePort = 4533;
-const useCloudFront = true;
+interface BlitStackProps extends cdk.StackProps {
+  vpsHost: string;
+  zoneName: string;
+  internal: string;
+  navidromePort: number;
+}
 
 export class BlitStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: BlitStackProps) {
     super(scope, id, props);
+
+    const { vpsHost, zoneName, internal, navidromePort } = props;
 
     const zone = new route53.PublicHostedZone(this, "BlitCC", {
       zoneName,
@@ -32,19 +36,6 @@ export class BlitStack extends cdk.Stack {
       recordName: internal,
       target: vpsTarget,
     });
-
-    if (!useCloudFront) {
-      new route53.ARecord(this, "BlitRoot", {
-        zone,
-        target: vpsTarget,
-      });
-
-      new route53.ARecord(this, "BlitNavidrone", {
-        zone,
-        recordName: "nd",
-        target: vpsTarget,
-      });
-    }
 
     new route53.TxtRecord(this, "BlitSPF", {
       zone,
@@ -74,68 +65,70 @@ export class BlitStack extends cdk.Stack {
       region: "us-east-1",
     });
 
-    if (useCloudFront) {
-      const distribution = new cloudfront.Distribution(this, "BlitFront", {
-        certificate,
-        domainNames: ["blit.cc"],
-        priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
-        defaultBehavior: {
-          origin: new origins.HttpOrigin(`${internal}.blit.cc`, {
-            protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
-            httpPort: internalPort,
-          }),
-          originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER,
-          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-          smoothStreaming: true,
-        },
-      });
+    new cdk.CfnOutput(this, "BlitCertArn", {
+      value: certificate.certificateArn,
+    });
 
-      new route53.ARecord(this, "BlitFrontRecord", {
+    const bucket = new s3.Bucket(this, "BlitBucket", {
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+    });
+
+    const distribution = new cloudfront.Distribution(this, "BlitFront", {
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+      defaultBehavior: {
+        origin: new origins.S3Origin(bucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      domainNames: ["blit.cc"],
+      certificate,
+    });
+
+    new s3deploy.BucketDeployment(this, "BlitDeployment", {
+      sources: [s3deploy.Source.asset("./public")],
+      destinationBucket: bucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+
+    new route53.ARecord(this, "BlitFrontRecord", {
+      zone,
+      target: route53.RecordTarget.fromAlias(new alias.CloudFrontTarget(distribution)),
+    });
+
+    // navidrome
+    const ndDistribution = new cloudfront.Distribution(this, "BlitFrontNavidrome", {
+      certificate,
+      domainNames: ["nd.blit.cc"],
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+      defaultBehavior: {
+        origin: new origins.HttpOrigin(`${internal}.blit.cc`, {
+          protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+          httpPort: navidromePort,
+        }),
+        cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+        originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+        smoothStreaming: true,
+      },
+    });
+
+    const ndZone = new route53.PublicHostedZone(this, "NDBlitCC", {
+      zoneName: `nd.${zoneName}`,
+    });
+
+    new route53.ARecord(this, "BlitFrontNavidromeRecord", {
+      zone: ndZone,
+      target: route53.RecordTarget.fromAlias(new alias.CloudFrontTarget(ndDistribution)),
+    });
+
+    // Delegate to subdomain
+    if (ndZone.hostedZoneNameServers) {
+      new route53.ZoneDelegationRecord(this, "BlitFrontNdDelegations", {
         zone,
-        target: route53.RecordTarget.fromAlias(
-          new alias.CloudFrontTarget(distribution)
-        ),
+        recordName: "nd",
+        nameServers: ndZone.hostedZoneNameServers,
       });
-
-      const ndDistribution = new cloudfront.Distribution(
-        this,
-        "BlitFrontNavidrome",
-        {
-          certificate,
-          domainNames: ["nd.blit.cc"],
-          priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
-          defaultBehavior: {
-            origin: new origins.HttpOrigin(`${internal}.blit.cc`, {
-              protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
-              httpPort: navidromePort,
-            }),
-            cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
-            originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER,
-            allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-            smoothStreaming: true,
-          },
-        }
-      );
-
-      const ndZone = new route53.PublicHostedZone(this, "NDBlitCC", {
-        zoneName: `nd.${zoneName}`,
-      });
-
-      new route53.ARecord(this, "BlitFrontNavidromeRecord", {
-        zone: ndZone,
-        target: route53.RecordTarget.fromAlias(
-          new alias.CloudFrontTarget(ndDistribution)
-        ),
-      });
-
-      // Delegate to subdomain
-      if (ndZone.hostedZoneNameServers) {
-        new route53.ZoneDelegationRecord(this, "BlitFrontNdDelegations", {
-          zone,
-          recordName: "nd",
-          nameServers: ndZone.hostedZoneNameServers,
-        });
-      }
     }
   }
 }
