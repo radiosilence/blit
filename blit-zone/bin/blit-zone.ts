6e6f7308aaa8b9a7925f660245baa69977d5a4c4
James Cleveland <jc@blit.cc>
2021-01-08 15:51:09 +0000

    move it back into one project

--- blit-zone/bin/blit-zone.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from "@aws-cdk/core";
-import "source-map-support/register";
-import { BlitZoneStack } from "../lib/blit-zone-stack";
-
-const app = new cdk.App();
-new BlitZoneStack(app, "BlitZoneStack", {
-  zoneName: "blit.cc",
-});
