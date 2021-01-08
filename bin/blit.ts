9fb61e91a691fa8d63dd352cfc6dd8971e6b8448
James Cleveland <jc@blit.cc>
2021-01-08 15:51:09 +0000

    move it back into one project

--- bin/blit.ts
+++ bin/blit.ts
@@ -2,16 +2,17 @@
 import * as cdk from "@aws-cdk/core";
 import "source-map-support/register";
 import { BlitWebStack } from "../lib/blit-web-stack";
+import { BlitZoneStack } from "../lib/blit-zone-stack";
 
 const app = new cdk.App();
 
-new BlitWebStack(app, "BlitStack", {
+new BlitWebStack(app, "BlitWebStack", {
   vpsIp: "149.91.89.243",
   zoneName: "blit.cc",
   internal: "xxpk4shiicfjldb50oiasudnas3nd",
   navidromePort: 4533,
-  env: {
-    region: "us-east-1",
-    account: "339435723451",
-  },
+});
+
+new BlitZoneStack(app, "BlitZoneStack", {
+  zoneName: "blit.cc",
 });
