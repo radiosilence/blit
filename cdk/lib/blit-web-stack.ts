686478a457f946d831fe840a9b31d96b3d09cf91
James Cleveland <jc@blit.cc>
2021-06-25 12:24:57 +0100

    add caching policy that includes qs

--- cdk/lib/blit-web-stack.ts
+++ cdk/lib/blit-web-stack.ts
@@ -2,6 +2,7 @@ import * as acm from "@aws-cdk/aws-certificatemanager";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
 import * as route53 from "@aws-cdk/aws-route53";
 import * as cdk from "@aws-cdk/core";
+import { Duration } from "@aws-cdk/core";
 import { StaticWeb } from "cdk-static-web";
 
 interface Props extends cdk.StackProps {
@@ -31,6 +32,16 @@ export class BlitWebStack extends cdk.Stack {
       defaultIndexes: true,
       distributionProps: {
         priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+      },
+      behaviourOptions: {
+        cachePolicy: new cloudfront.CachePolicy(this, "CachePolicy", {
+          minTtl: Duration.seconds(1),
+          maxTtl: Duration.seconds(31536000),
+          defaultTtl: Duration.seconds(86400),
+          queryStringBehavior: cloudfront.CacheQueryStringBehavior.all(),
+          headerBehavior: cloudfront.CacheHeaderBehavior.none(),
+          cachePolicyName: "CachingWithQueryString",
+        }),
       },
       errorPagePath: "/{CODE}/index.html",
     });
