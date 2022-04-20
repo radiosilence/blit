db94cc7b32434d0d6894bbc8bdc8d365cff8cd07
James Cleveland <james.cleveland@engineerbetter.com>
2022-04-20 16:50:19 +0100

    updating for funs

---
+++ web/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{astro,js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Consolas", "sans-serif"],
+      serif: ["Consolas", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
