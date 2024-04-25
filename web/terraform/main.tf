8f52f558d69658bfc3e80bbcf336efe81656ff44
James Cleveland <jc@blit.cc>
2024-04-25 11:49:11 +0100

    basic terraform for ec2
    make for terraform
    
    more terraform& outputs
    
    configure with tf cloud
    
    basic TF apply stuff without vars yet
    
    remove tf dirs

---
+++ web/terraform/main.tf
@@ -0,0 +1,103 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+
+  cloud {
+    organization = "crack-squirrels"
+
+    workspaces {
+      name = "blit-web"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_egress_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_instance" "instance" {
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.allow_http.name]
+  tags = {
+    Name = var.tag_name
+  }
+}
+
+resource "aws_cloudfront_distribution" "cf" {
+  aliases      = [var.hostname]
+  price_class  = var.price_class
+  enabled      = true
+  http_version = "http2and3"
+
+  origin {
+    origin_id   = "blit-web-ec2"
+    domain_name = aws_instance.instance.public_dns
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "blit-web-ec2"
+
+    compress = true
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = var.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
