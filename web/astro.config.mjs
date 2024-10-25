35761dd70e6680036e4e457abd297699249c9618
James Cleveland <jc@blit.cc>
2024-10-25 22:54:40 +0100

    ooh thatll break

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,6 +1,5 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [tailwind()],
