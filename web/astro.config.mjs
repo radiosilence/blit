302cdec41836220c6139b62eb5dd0aef7b86fb7e
James Cleveland <jc@blit.cc>
2024-10-25 23:20:53 +0100

    prefetching

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,5 +2,8 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
   integrations: [tailwind()],
 });
