b84fc4e1976a1b9a6070b26829d03c15920d450b
James Cleveland <jc@blit.cc>
2024-10-25 22:51:24 +0100

    use a share compoenetn for logo

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,6 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [tailwind()],
