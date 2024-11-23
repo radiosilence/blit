8633b891ed3c7a44eaab92848901131f3eeefd72
James Cleveland <jc@blit.cc>
2024-11-23 14:13:43 +0000

    simpler version

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,7 +43,7 @@ resource "cloudflare_record" "github_pages_a" {
   proxied  = false
   ttl      = 1
   type     = "A"
-  content  = each.value
+  content  = each.key
   for_each = var.github_a_records
   zone_id  = cloudflare_zone.zone.id
 }
@@ -59,24 +59,24 @@ resource "cloudflare_record" "github_pages_a_www" {
 
 resource "cloudflare_record" "record_fm_dk" {
   for_each = toset(["fm1", "fm2", "fm3", "fm4"])
-  name     = "${each.value}._domainkey"
+  name     = "${each.key}._domainkey"
   proxied  = false
   ttl      = 1
   type     = "CNAME"
-  content  = "${each.value}.${var.cloudflare_zone}.dkim.fmhosted.com"
+  content  = "${each.key}.${var.cloudflare_zone}.dkim.fmhosted.com"
   zone_id  = cloudflare_zone.zone.id
 }
 
 resource "cloudflare_record" "record_fm_mx" {
-  for_each = toset([
-    { name = "in1", priority = 10 },
-    { name = "in2", priority = 20 },
-  ])
+  for_each = map({
+    "in1" = 10,
+    "in2" = 20,
+  })
   name     = var.cloudflare_zone
-  priority = each.value.priority
+  priority = each.value
   ttl      = 1
   type     = "MX"
-  content  = "${each.value.name}-smtp.messagingengine.com"
+  content  = "${each.key}-smtp.messagingengine.com"
   zone_id  = cloudflare_zone.zone.id
 }
 
