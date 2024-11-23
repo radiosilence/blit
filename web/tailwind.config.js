c99ca2a76f5d9b70fd4576ae8a387ffb3ec6b3e4
James Cleveland <jc@blit.cc>
2024-11-23 13:04:55 +0000

    functional dark mode

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,3 +1,21 @@
+const proseVars = [
+  "--tw-prose-body",
+  "--tw-prose-headings",
+  "--tw-prose-lead",
+  "--tw-prose-bold",
+  "--tw-prose-counters",
+  "--tw-prose-bullets",
+  "--tw-prose-hr",
+  "--tw-prose-quotes",
+  "--tw-prose-quote-borders",
+  "--tw-prose-captions",
+  "--tw-prose-code",
+  "--tw-prose-pre-code",
+  "--tw-prose-pre-bg",
+  "--tw-prose-th-borders",
+  "--tw-prose-td-borders",
+];
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{astro,js,ts,jsx,tsx,md}"],
@@ -10,23 +28,19 @@ module.exports = {
       colors: {
         brand: {
           DEFAULT: "#e65e5e",
-          light: "#f7d4d4",
+          light: "#ccc",
           mid: "#666666",
           dark: "#333",
         },
       },
-      typography: {
+      typography: ({ theme }) => ({
         DEFAULT: {
           css: {
-            a: {
-              color: "#e65e5e",
-              "&:hover": {
-                color: "#666666",
-              },
-            },
+            "--tw-prose-links": theme("colors.brand.DEFAULT"),
+            ...proseVars.reduce((acc, key) => ({ ...acc, [key]: "inherit" }), {}),
           },
         },
-      },
+      }),
     },
   },
   plugins: [require("@tailwindcss/typography")],
