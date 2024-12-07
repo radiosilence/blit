45292828c2fb29d124e907dec464d70858c3aae0
James Cleveland <jc@blit.cc>
2024-12-07 12:04:26 +0000

    remove old tw stuff

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,9 +1,7 @@
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   prefetch: {
     defaultStrategy: "viewport",
   },
-  // integrations: [tailwind()],
 });
