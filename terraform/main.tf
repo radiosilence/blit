792ac9315dd273151e837d2c5564f514817e7ad5
James Cleveland <jc@blit.cc>
2024-12-12 13:42:45 +0000

    inject zone ids as we cant change them

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,72 +21,46 @@ provider "cloudflare" {
 
 # Blit
 
-# Blit Zone
-resource "cloudflare_zone" "zone" {
-  account_id = var.cloudflare_account_id
-  zone       = var.blit_domain
-}
-
-locals {
-  blit_zone = {
-    id   = cloudflare_zone.blit.id
-    name = var.blit_domain
-  }
-}
-
 # Blit Let's Encrypt
 module "blit_letsencrypt" {
   source = "./modules/letsencrypt"
-  zone   = local.blit_zone
+  zone   = var.blit_zone
 }
 
 # Blit Fastmail
 module "blit_fastmail" {
   source = "./modules/fastmail"
-  zone   = local.blit_zone
+  zone   = var.blit_zone
 }
 
 # Blit GitHub
 module "blit_github" {
   source = "./modules/github"
-  zone   = local.blit_zone
+  zone   = var.blit_zone
 }
 
 # Blit Bluesky
 module "blit_bluesky" {
   source = "./modules/bluesky"
-  zone   = local.blit_zone
+  zone   = var.blit_zone
 }
 
 # Buttholes
 
-# Buttholes Zone
-resource "cloudflare_zone" "buttholes_zone" {
-  account_id = var.cloudflare_account_id
-  zone       = var.buttholes_domain
-}
-
-locals {
-  buttholes_zone = {
-    id   = cloudflare_zone.buttholes_zone.id
-    name = var.blit_domain
-  }
-}
-
 # Buttholes Let's Encrypt
 module "buttholes_letsencrypt" {
   source = "./modules/letsencrypt"
-  zone   = local.buttholes_zone
+  zone   = var.buttholes_zone
 }
 
 # Buttholes Fastmail
 module "buttholes_fastmail" {
   source = "./modules/fastmail"
-  zone   = local.buttholes_zone
+  zone   = var.buttholes_zone
 }
 
 # Buttholes Bsky
 module "buttholes_bluesky" {
   source = "./modules/bluesky"
-  zone   = local.buttholes_zone
+  zone   = var.buttholes_zone
 }
