8081be1c6ee84d8bd424308ecf40ff63e98ca160
James Cleveland <jc@blit.cc>
2024-12-12 12:29:15 +0000

    lets try

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,14 +12,57 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-
-resource "cloudflare_zone_blit" "zone" {
+# Blit Zone
+resource "blit_cloudflare_zone" "zone" {
   account_id = var.cloudflare_account_id
-  zone       = var.domain_blit
+  zone       = var.blit_domain
+}
+
+# Blit Let's Encrypt
+module "blit_letsencrypt" {
+  source  = "./modules/letsencrypt"
+  zone_id = blit_cloudflare_zone.zone.id
+  domain  = var.blit_domain
+}
+
+# Blit Fastmail
+module "blit_fastmail" {
+  source  = "./modules/fastmail"
+  zone_id = blit_cloudflare_zone.zone.id
+  domain  = var.blit_domain
+}
+
+# Blit GitHub
+module "blit_github" {
+  source  = "./modules/github"
+  zone_id = blit_cloudflare_zone.zone.id
+  domain  = var.blit_domain
 }
 
 
-resource "cloudflare_zone_buttholes" "zone" {
+# Blit Bluesky
+module "blit_bluesky" {
+  source  = "./modules/bluesky"
+  zone_id = blit_cloudflare_zone.zone.id
+}
+
+# Buttholes Zone
+resource "buttholes_cloudflare_zone" "zone" {
   account_id = var.cloudflare_account_id
-  zone       = var.domain_buttholes
+  zone       = var.buttholes_domain
+}
+
+# Buttholes Let's Encrypt
+
+# Buttholes Fastmail
+module "buttholes_fastmail" {
+  source  = "./modules/fastmail"
+  zone_id = buttholes_cloudflare_zone.zone.id
+  domain  = var.buttholes_domain
+}
+
+# Buttholes Bsky
+module "buttholes_bluesky" {
+  source  = "./modules/bluesky"
+  zone_id = buttholes_cloudflare_zone.zone.id
 }
