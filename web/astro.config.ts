94d1429617cc0ee690d773177fc248f7d0926148
James Cleveland <james.cleveland@fresha.com>
2025-02-16 18:04:28 +0000

    use the astro plugin

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -1,7 +1,8 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
 export default defineConfig({
   prefetch: {
     defaultStrategy: "viewport",
   },
+  vite: { plugins: [tailwindcss()] },
 });
