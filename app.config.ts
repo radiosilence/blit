eeb167b9b43880fc5aec92e515abe400fe9566ab
James Cleveland <jc@blit.cc>
2025-05-26 16:16:26 +0100

    tanstack start!

---
+++ app.config.ts
@@ -0,0 +1,24 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "@tanstack/react-start/config";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  tsr: {
+    appDirectory: "app",
+  },
+  server: {
+    preset: "bun",
+    prerender: {
+      routes: ["/", "/cv"],
+      crawlLinks: true,
+    },
+  },
+  vite: {
+    plugins: [
+      tsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+      tailwindcss(),
+    ],
+  },
+});
---
+++ app/routes/index.tsx
@@ -0,0 +1,20 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { Logo } from "@/components/logo";
+
+export const Route = createFileRoute("/")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <section className="flex flex-col items-center m-12 space-y-4 text-center">
+      <Logo width={256} className="mt-12 mb-8" />
+      <h1>james cleveland</h1>
+      <p className="text-sm">full stack / react native / devops</p>
+      <p>
+        <a href="/cv">cv-2025.01</a> /{" "}
+        <a href="https://github.com/radiosilence">github</a>
+      </p>
+    </section>
+  );
+}
