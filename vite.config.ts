b22218613811807709cdd251638073fcae390c04
James Cleveland <james.cleveland@fresha.com>
2025-06-12 21:45:56 +0100

    using bun

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     }),
     tailwindcss(),
     tanstackStart({
-      target: "static",
+      target: "bun",
       prerender: {
         failOnError: true,
         enabled: true,
