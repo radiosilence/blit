aeb249a2ead5b6a7a6ff934de180eec84cbdeaad
James Cleveland <jc@blit.cc>
2025-06-13 20:55:28 +0100

    what if I put cv in a loader

--- src/routes/cv.tsx
+++ src/routes/cv.tsx
@@ -6,16 +6,21 @@ import cv from "../assets/cv.md";
 
 export const Route = createFileRoute("/cv")({
   component: RouteComponent,
+  loader: () => ({
+    cv: <Markdown rehypePlugins={[rehypeRaw]}>{cv}</Markdown>,
+  }),
+  staleTime: Infinity,
 });
 
 function RouteComponent() {
+  const { cv } = Route.useLoaderData();
   return (
     <section className="flex items-center flex-col mb-24 px-4 lg:px-0">
       <article className="prose prose-sm">
         <a href="/">
           <Logo width={128} className="mt-12 mb-16" />
         </a>
-        <Markdown rehypePlugins={[rehypeRaw]}>{cv}</Markdown>
+        {cv}
       </article>
     </section>
   );
