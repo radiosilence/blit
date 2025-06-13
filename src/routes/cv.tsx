bf5c5610484495fca67d104cf729a512a4316e9e
James Cleveland <jc@blit.cc>
2025-06-13 20:57:45 +0100

    it didnot work

--- src/routes/cv.tsx
+++ src/routes/cv.tsx
@@ -1,26 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Markdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
+import cv from "~/assets/cv.md";
 import { Logo } from "~/components/logo";
-import cv from "../assets/cv.md";
 
 export const Route = createFileRoute("/cv")({
   component: RouteComponent,
-  loader: () => ({
-    cv: <Markdown rehypePlugins={[rehypeRaw]}>{cv}</Markdown>,
-  }),
-  staleTime: Infinity,
 });
 
 function RouteComponent() {
-  const { cv } = Route.useLoaderData();
   return (
     <section className="flex items-center flex-col mb-24 px-4 lg:px-0">
       <article className="prose prose-sm">
         <a href="/">
           <Logo width={128} className="mt-12 mb-16" />
         </a>
-        {cv}
+        <Markdown rehypePlugins={[rehypeRaw]}>{cv}</Markdown>
       </article>
     </section>
   );
