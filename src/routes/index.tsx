86c68cb5d1d361364b3c00f65fe21811ba5286fc
James Cleveland <jc@blit.cc>
2025-07-29 12:17:32 +0100

    logical tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute("/")({
 function RouteComponent() {
   return (
     <section className="flex flex-col items-center m-12 space-y-4 text-center">
-      <Logo width={256} className="mt-12 mb-8" />
+      <Logo width={256} className="mbs-12 mbe-8" />
       <h1>james cleveland</h1>
       <p className="text-sm">senior full stack engineer</p>
       <p>
