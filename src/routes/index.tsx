538c55d2f008cd03252c6c0aed178fb7e1ae1976
James Cleveland <jc@blit.cc>
2025-09-26 15:27:34 +0100

    more locales

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -35,6 +35,26 @@ function RouteComponent() {
         <a href="/ja-JP/cv">履歴書</a>
         <a href="/zh-CN/">中文</a>
         <a href="/zh-CN/cv">简历</a>
+        <a href="/vi-VN/">Tiếng Việt</a>
+        <a href="/vi-VN/cv">Sơ yếu lý lịch</a>
+        <a href="/ka-GE/">ქართული</a>
+        <a href="/ka-GE/cv">რეზიუმე</a>
+        <a href="/uk-UA/">Українська</a>
+        <a href="/uk-UA/cv">Резюме</a>
+        <a href="/ar-PS/">العربية الفلسطينية</a>
+        <a href="/ar-PS/cv">السيرة الذاتية</a>
+        <a href="/it-IT/">Italiano</a>
+        <a href="/it-IT/cv">Curriculum</a>
+        <a href="/de-DE/">Deutsch</a>
+        <a href="/de-DE/cv">Lebenslauf</a>
+        <a href="/nl-BE/">Nederlands (België)</a>
+        <a href="/nl-BE/cv">Curriculum Vitae</a>
+        <a href="/nl-NL/">Nederlands</a>
+        <a href="/nl-NL/cv">CV</a>
+        <a href="/pl-PL/">Polski</a>
+        <a href="/pl-PL/cv">Życiorys</a>
+        <a href="/cs-CZ/">Čeština</a>
+        <a href="/cs-CZ/cv">Životopis</a>
       </div>
     </section>
   );
