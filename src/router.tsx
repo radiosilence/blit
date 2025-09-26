a1fab3731394a21487b74eb58bd79874ea43269a
James Cleveland <jc@blit.cc>
2025-09-26 14:40:48 +0100

    duno what that does

--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,9 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-export function createRouter() {
-  const router = createTanStackRouter({
+export const getRouter = () =>
+  createTanStackRouter({
     routeTree,
     defaultPreload: "intent",
     scrollRestoration: true,
   });
-
-  return router;
-}
-
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: ReturnType<typeof createRouter>;
-  }
-}
