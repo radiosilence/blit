70edc16b0b8c8e088d6300b34f587a6e6b93deeb
James Cleveland <jc@blit.cc>
2025-09-26 16:09:01 +0100

    doin paraglide

--- src/routes/$locale/cv.tsx
+++ src/routes/$locale/cv.tsx
@@ -1,13 +1,34 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { i18n, isValidLocale } from "~/lib/i18n";
+import { setLanguageTag } from "~/paraglide/runtime";
+
+const supportedLocales = [
+  "en-GB",
+  "fr-FR",
+  "ar",
+  "ja-JP",
+  "zh-CN",
+  "ka-GE",
+  "uk-UA",
+  "ar-PS",
+  "it-IT",
+  "de-DE",
+  "nl-BE",
+  "nl-NL",
+  "pl-PL",
+] as const;
+type SupportedLocale = (typeof supportedLocales)[number];
+
+const isValidLocale = (locale: string): locale is SupportedLocale => {
+  return (supportedLocales as readonly string[]).includes(locale);
+};
 
 export const Route = createFileRoute("/$locale/cv")({
   beforeLoad: async ({ params }: { params: { locale: string } }) => {
     if (!isValidLocale(params.locale)) {
       throw new Error(`Invalid locale: ${params.locale}`);
     }
-    // Set language for SSR - wait for it to complete
-    await i18n.changeLanguage(params.locale);
+    // Set paraglide language tag for SSR
+    setLanguageTag(params.locale);
   },
   component: () => <div>CV page for locale</div>,
 });
