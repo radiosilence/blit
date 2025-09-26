e6a3b0e6cc7a8a4810551c49eeaf44f034d9f43d
James Cleveland <jc@blit.cc>
2025-09-26 16:22:14 +0100

    unfuck cv page

--- src/routes/$locale/cv.tsx
+++ src/routes/$locale/cv.tsx
@@ -1,35 +1,20 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { setLanguageTag } from "~/paraglide/runtime";
-
-const supportedLanguageTags = [
-  "en-GB",
-  "fr-FR",
-  "ar",
-  "ja-JP",
-  "zh-CN",
-  "ka-GE",
-  "uk-UA",
-  "ar-PS",
-  "it-IT",
-  "de-DE",
-  "nl-BE",
-  "nl-NL",
-  "pl-PL",
-] as const;
-type SupportedLanguageTag = (typeof supportedLanguageTags)[number];
-
-const isValidLanguageTag = (
-  languageTag: string,
-): languageTag is SupportedLanguageTag => {
-  return (supportedLanguageTags as readonly string[]).includes(languageTag);
-};
+import CV from "~/components/cv.mdx";
+import { Logo } from "~/components/logo";
 
 export const Route = createFileRoute("/$locale/cv")({
-  beforeLoad: ({ params }: { params: { locale: string } }) => {
-    if (!isValidLanguageTag(params.locale)) {
-      throw new Error(`Invalid language tag: ${params.locale}`);
-    }
-    setLanguageTag(params.locale);
-  },
-  component: () => <div>CV page for locale</div>,
+  component: RouteComponent,
 });
+
+function RouteComponent() {
+  return (
+    <section className="flex items-center flex-col mbe-24 px-4 lg:px-0">
+      <article className="prose prose-sm">
+        <a href="/">
+          <Logo width={128} className="mbs-12 mbe-16" />
+        </a>
+        <CV />
+      </article>
+    </section>
+  );
+}
