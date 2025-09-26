d7760f3f357c0582763c9d677bb320b75ff4023b
James Cleveland <jc@blit.cc>
2025-09-26 17:26:27 +0100

    dont try and pull remote plugins

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,6 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  build: {
-    rollupOptions: {
-      external: ["parsimmon"],
-    },
-  },
   plugins: [
     { enforce: "pre", ...mdx() },
     paraglide({
