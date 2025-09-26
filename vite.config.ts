9f9f9a582b1df57e741a2b8779ce1d0566cdedc6
James Cleveland <jc@blit.cc>
2025-09-26 17:16:50 +0100

    externalise that mod

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,11 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    rollupOptions: {
+      external: ["parsimmon"],
+    },
+  },
   plugins: [
     { enforce: "pre", ...mdx() },
     paraglide({
