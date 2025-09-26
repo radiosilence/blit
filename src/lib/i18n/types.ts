4446f532ef438e28a7af91f7da36b1a125c78a5b
James Cleveland <jc@blit.cc>
2025-09-26 15:27:34 +0100

    more locales

--- src/lib/i18n/types.ts
+++ src/lib/i18n/types.ts
@@ -4,13 +4,23 @@ export const supportedLocales = [
   "ar",
   "ja-JP",
   "zh-CN",
+  "vi-VN",
+  "ka-GE",
+  "uk-UA",
+  "ar-PS",
+  "it-IT",
+  "de-DE",
+  "nl-BE",
+  "nl-NL",
+  "pl-PL",
+  "cs-CZ",
 ] as const;
 export type SupportedLocale = (typeof supportedLocales)[number];
 
 export const defaultLocale: SupportedLocale = "en-GB";
 
 // RTL languages
-export const rtlLocales: readonly SupportedLocale[] = ["ar"] as const;
+export const rtlLocales: readonly SupportedLocale[] = ["ar", "ar-PS"] as const;
 
 export function isValidLocale(locale: string): locale is SupportedLocale {
   return supportedLocales.includes(locale as SupportedLocale);
