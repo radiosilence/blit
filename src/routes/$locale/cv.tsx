a50e64bc797c800188ebf1df6a3d41628779f311
James Cleveland <jc@blit.cc>
2025-09-26 15:49:36 +0100

    well I guess that works

--- src/routes/$locale/cv.tsx
+++ src/routes/$locale/cv.tsx
@@ -1,11 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { isValidLocale } from "~/lib/i18n";
+import { i18n, isValidLocale } from "~/lib/i18n";
 
 export const Route = createFileRoute("/$locale/cv")({
-  beforeLoad: ({ params }: { params: { locale: string } }) => {
+  beforeLoad: async ({ params }: { params: { locale: string } }) => {
     if (!isValidLocale(params.locale)) {
       throw new Error(`Invalid locale: ${params.locale}`);
     }
+    // Set language for SSR - wait for it to complete
+    await i18n.changeLanguage(params.locale);
   },
   component: () => <div>CV page for locale</div>,
 });
