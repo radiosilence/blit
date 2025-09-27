a15733b803e52182e7a3455e15db6ebf20d7d35b
James Cleveland <jc@blit.cc>
2025-09-27 12:11:13 +0100

    astro version

--- src/router.tsx
+++
@@ -1,9 +0,0 @@
-import { createRouter as createTanStackRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
-
-export const getRouter = () =>
-  createTanStackRouter({
-    routeTree,
-    defaultPreload: "intent",
-    scrollRestoration: true,
-  });
