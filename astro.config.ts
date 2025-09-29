46de701da06b645558564a1beb1c1f83625ab717
James Cleveland <jc@blit.cc>
2025-09-29 16:41:35 +0100

    fi xlog ting

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,14 @@
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import { lingui } from "@lingui/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
   vite: {
-    plugins: [tailwindcss(), lingui() as any],
+    // biome-ignore lint/suspicious/noExplicitAny: lingui is a dick
+    plugins: [tailwindcss(), lingui() as unknown as any, react()],
   },
 });
