95a1c3c44feb617e0417a2e293bb2c92263a637b
James Cleveland <jc@blit.cc>
2025-09-29 16:08:03 +0100

    remove messages file

--- src/messages.ts
+++
@@ -1,9 +0,0 @@
-// Message ID constants (generated from Lingui extraction)
-export const MSG = {
-  SITE_TITLE: "WHoYMd", // "james cleveland : senior full stack engineer"
-  NAME: "xXnTxT", // "james cleveland"
-  TITLE: "s807BJ", // "senior full stack engineer"
-  CV_LINK: "mSuX4i", // "cv-2025.01"
-  GITHUB_LINK: "bxdQfV", // "github"
-  SITE_NAME: "VrQ+UK", // "blit.cc"
-} as const;
