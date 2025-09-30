732bdeb888acd9423bde70d9411cdd9850f78198
James Cleveland <jc@blit.cc>
2025-09-30 10:45:28 +0100

    nicer middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,11 @@ import { defineMiddleware } from "astro/middleware";
 import { initI18n } from "./i18n";
 
 export const onRequest = defineMiddleware(async (context, next) => {
-  // Extract locale from URL: /fr-FR/page -> "fr-FR"
-  const url = new URL(context.request.url);
-  const locale = url.pathname.split("/").filter(Boolean)[0] || "en-GB";
+  const [locale] = new URL(context.request.url).pathname
+    .split("/")
+    .filter((part) => !!part);
 
-  await initI18n(locale);
+  await initI18n(locale ?? "en-GB");
 
   return next();
 });
