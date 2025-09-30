9b334a64f348c2cc323146db3086c44701aaf922
James Cleveland <jc@blit.cc>
2025-09-30 07:36:51 +0100

    get rid of pointless pickers

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,8 +1,6 @@
 import { i18n } from "@lingui/core";
 import Logo from "@/components/Logo.tsx";
 
-const { locale } = i18n;
-
 const supportedLocales = [
   "en-GB",
   "fr-FR",
@@ -20,39 +18,31 @@ const supportedLocales = [
 ];
 
 // Server-side rendered content
-const ServerContent = () => (
-  <section className="flex flex-col items-center m-12 space-y-4 text-center">
-    <Logo width={256} className="mbs-12 mbe-8" />
-    <h1>{i18n._("james cleveland")}</h1>
-    <p className="text-sm">
-      {i18n._("james cleveland : senior full stack engineer")}
-    </p>
-    <p>
-      <a href={`${locale !== "en-GB" ? `/${locale}` : ""}/cv`}>
-        {i18n._("cv-2025.01")}
-      </a>
-      {" / "}
-      <a href="https://github.com/radiosilence">{i18n._("github")}</a>
-      <div className="hidden">
-        {supportedLocales.map((locale) => (
-          <div key={locale}>
-            <a href={`/${locale}`}>{locale}</a>
-            <a href={`/${locale}/cv`}>{locale} CV</a>
-          </div>
-        ))}
-      </div>
-      {/* Fallback language selector for SSR */}
-      <div className="picker">
-        <select className="bg-transparent text-xs cursor-pointer">
-          {supportedLocales.map((locale) => (
-            <option key={locale} value={locale}>
-              {locale}
-            </option>
-          ))}
-        </select>
-      </div>
-    </p>
-  </section>
-);
+export const HomePage = () => {
+  const { locale } = i18n;
 
-export const HomePage = ServerContent;
+  return (
+    <section className="flex flex-col items-center m-12 space-y-4 text-center">
+      <Logo width={256} className="mbs-12 mbe-8" />
+      <h1>{i18n._("james cleveland")}</h1>
+      <p className="text-sm">
+        {i18n._("james cleveland : senior full stack engineer")}
+      </p>
+      <p>
+        <a href={`${locale !== "en-GB" ? `/${locale}` : ""}/cv`}>
+          {i18n._("cv-2025.01")}
+        </a>
+        {" / "}
+        <a href="https://github.com/radiosilence">{i18n._("github")}</a>
+        <div className="hidden">
+          {supportedLocales.map((locale) => (
+            <div key={locale}>
+              <a href={`/${locale}`}>{locale}</a>
+              <a href={`/${locale}/cv`}>{locale} CV</a>
+            </div>
+          ))}
+        </div>
+      </p>
+    </section>
+  );
+};
