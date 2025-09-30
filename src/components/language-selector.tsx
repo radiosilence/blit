9520167c4940fbfca5be09c45382a40939d95798
James Cleveland <jc@blit.cc>
2025-09-30 15:38:23 +0100

    grhh

--- src/components/language-selector.tsx
+++ src/components/language-selector.tsx
@@ -1,12 +1,21 @@
 import { i18n } from "@lingui/core";
-import type { ComponentType } from "react";
+import { I18nProvider, useLingui } from "@lingui/react";
+import type { ComponentProps } from "astro/types";
+import { type ComponentType, useEffect, useRef } from "react";
 
-export const LanguageSelector: ComponentType<{
+const locale = "fr-FR";
+
+console.log("activating", locale);
+
+const LanguageSelectorInner: ComponentType<{
   locales: string[];
   sourceLocale: string;
 }> = ({ locales, sourceLocale }) => {
+  const { i18n } = useLingui();
+  console.log("LanguageSelectorInner::", i18n.locale);
   return (
     <div className="picker">
+      {i18n._("SELECT LANGUAGE:")}
       <select
         value={i18n.locale}
         onChange={(e) => {
@@ -25,4 +34,29 @@ export const LanguageSelector: ComponentType<{
   );
 };
 
-export default LanguageSelector;
+export function withLanguage<T extends ComponentType>(C: T) {
+  return ({ locale, ...props }: ComponentProps<T> & { locale: string }) => {
+    const activated = useRef(false);
+    console.log("LanguageSelector::render");
+    (async () => {
+      console.log("LanguageSelector::doing load and activate");
+      if (activated.current) return;
+      activated.current = true;
+      i18n.loadAndActivate({
+        locale,
+        ...(await import(`../locales/${locale}/messages.mjs`)),
+      });
+    })();
+    return (
+      <I18nProvider i18n={i18n}>
+        <C {...props} />
+      </I18nProvider>
+    );
+  };
+}
+
+export const LanguageSelector = withLanguage(LanguageSelectorInner);
+
+const Balh = () => {
+  return <LanguageSelector locale="fr-FR" sourceLocale="en-GB" locales={[]} />;
+};
