1fd03fedf8e708b8d36d606fc0f3587e816d4d4f
James Cleveland <jc@blit.cc>
2026-05-10 15:07:30 +0100

    just because you can

--- src/components/home-link.tsx
+++
@@ -1,9 +0,0 @@
-import { sourceLocale } from "#/i18n/config.ts";
-import { useLingui } from "@lingui/react";
-import type { PropsWithChildren } from "react";
-
-export function HomeLink({ children }: PropsWithChildren) {
-  const { i18n } = useLingui();
-
-  return <a href={i18n.locale !== sourceLocale ? `/${i18n.locale}/` : "/"}>{children}</a>;
-}
