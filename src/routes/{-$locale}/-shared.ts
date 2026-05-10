3e701ea6ccc213a1609c48c854fb077543f1c815
James Cleveland <jc@blit.cc>
2026-05-10 15:47:45 +0100

    eyy

--- src/routes/{-$locale}/-shared.ts
+++ src/routes/{-$locale}/-shared.ts
@@ -3,12 +3,12 @@ import { notFound, redirect } from "@tanstack/react-router";
 import { loadCatalog } from "#/i18n/catalogs";
 import { isValidLocale, sourceLocale } from "#/i18n/config";
 
-export function localeLoader(locale: string = sourceLocale) {
+export function localeLoader(locale: string | undefined = sourceLocale) {
   if (!isValidLocale(locale)) throw notFound();
   return loadCatalog(locale);
 }
 
-export function maybeStripSourceLocale(locale: string) {
+export function maybeStripSourceLocale(locale: string | undefined) {
   if (locale === sourceLocale)
     throw redirect({
       params: (prev) => ({ ...prev, locale: undefined }),
