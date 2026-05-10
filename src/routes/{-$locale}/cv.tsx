be5c1e6284932db8658f267afd96cbb433f570bb
James Cleveland <jc@blit.cc>
2026-05-10 12:40:40 +0100

    use #/

--- src/routes/{-$locale}/cv.tsx
+++ src/routes/{-$locale}/cv.tsx
@@ -1,10 +1,11 @@
-import { CvContent } from "@/components/cv-content";
+import { CvContent } from "#/components/cv-content";
 import { localeLoader } from "./-shared.ts";
 import { createFileRoute } from "@tanstack/react-router";
+import { sourceLocale } from "#/i18n/config.ts";
 
 export const Route = createFileRoute("/{-$locale}/cv")({
   loader: ({ params }) => {
-    localeLoader(params.locale ?? "en-GB");
+    localeLoader(params.locale ?? sourceLocale);
   },
   component: CvContent,
 });
--- src/routes/{-$locale}/index.tsx
+++ src/routes/{-$locale}/index.tsx
@@ -1,4 +1,4 @@
-import { HomeContent } from "@/components/home-content";
+import { HomeContent } from "#/components/home-content";
 import { localeLoader } from "./-shared.ts";
 import { createFileRoute } from "@tanstack/react-router";
 
