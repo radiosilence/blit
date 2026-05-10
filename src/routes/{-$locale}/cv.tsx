be32dca90b91d1d00f8ecb4e8489fa2fe9defd1a
James Cleveland <jc@blit.cc>
2026-05-10 13:08:00 +0100

    lol mdx

--- src/routes/{-$locale}/cv.tsx
+++ src/routes/{-$locale}/cv.tsx
@@ -1,11 +1,11 @@
-import { CvContent } from "#/components/cv-content";
-import { localeLoader } from "./-shared.ts";
 import { createFileRoute } from "@tanstack/react-router";
 import { sourceLocale } from "#/i18n/config.ts";
+import CV from "#/components/cv.mdx";
+import { localeLoader } from "./-shared.ts";
 
 export const Route = createFileRoute("/{-$locale}/cv")({
   loader: ({ params }) => {
     localeLoader(params.locale ?? sourceLocale);
   },
-  component: CvContent,
+  component: CV,
 });
